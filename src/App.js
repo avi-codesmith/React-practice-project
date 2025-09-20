269711f55fcc7ed14572017ba1181e2034a3534f
avi-codesmith <avi.codesmith@gmail.com>
2025-09-20 23:33:51 +0530

    Implement with Props

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import CoreLogo from "./bulb.svg";
 import PropsLogo from "./pops.svg";
 import DomLogo from "./dom.svg";
 
-const PropHeading = ["Components", "State & Props", "Virtual DOM"];
-
-const PropParagraphs = [
-  "Components are the building blocks of any React application. They let you break the UI into reusable pieces, making your code organized, modular, and easier to maintain. Each component can have its own structure, logic, and style.",
-
-  "State and Props are fundamental for managing dynamic data in React. State holds data that can change over time within a component, while Props allow you to pass data from parent components to child components, enabling component reusability and flexibility.",
-
-  "The Virtual DOM is a lightweight copy of the actual DOM that React uses to optimize UI updates. When the state changes, React updates the Virtual DOM first, compares it with the real DOM, and efficiently updates only the necessary parts, improving performance and user experience.",
+const CoreData = [
+  {
+    src: CoreLogo,
+    heading: "Components",
+    para: "Components are the building blocks of any React application. They let you break the UI into reusable pieces, making your code organized, modular, and easier to maintain. Each component can have its own structure, logic, and style.",
+  },
+  {
+    src: PropsLogo,
+    heading: "State & Props",
+    para: "State and Props are fundamental for managing dynamic data in React. State holds data that can change over time within a component, while Props allow you to pass data from parent components to child components, enabling component reusability and flexibility.",
+  },
+  {
+    src: DomLogo,
+    heading: "Virtual DOM",
+    para: "The Virtual DOM is a lightweight copy of the actual DOM that React uses to optimize UI updates. When the state changes, React updates the Virtual DOM first, compares it with the real DOM, and efficiently updates only the necessary parts, improving performance and user experience.",
+  },
 ];
 
 function Header() {
@@ -44,20 +52,12 @@ function App() {
       <h1 class="core-heading"> Core Concepts</h1>
       <div class="core-concept-container">
         <CoreConcept
-          src={CoreLogo}
-          heading={PropHeading[0]}
-          para={[PropParagraphs[0]]}
+          src={CoreData[0].src}
+          heading={CoreData[0].heading}
+          para={CoreData[0].para}
         />
-        <CoreConcept
-          src={PropsLogo}
-          heading={PropHeading[1]}
-          para={[PropParagraphs[1]]}
-        />
-        <CoreConcept
-          src={DomLogo}
-          heading={PropHeading[2]}
-          para={[PropParagraphs[2]]}
-        />
+        <CoreConcept {...CoreData[1]} />
+        <CoreConcept {...CoreData[2]} />
       </div>
     </div>
   );
