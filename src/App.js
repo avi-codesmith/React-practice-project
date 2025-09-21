9f9e1de705f3999ecf37ed0fc5eb2113732c8aba
avi-codesmith <avi.codesmith@gmail.com>
2025-09-21 12:31:38 +0530

    Implement in Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import "./App.css";
 import ReactLogo from "./logo.svg";
-import CoreLogo from "./bulb.svg";
-import PropsLogo from "./pops.svg";
-import DomLogo from "./dom.svg";
-
-const CoreData = [
-  {
-    src: CoreLogo,
-    heading: "Components",
-    para: "Components are the building blocks of any React application. They let you break the UI into reusable pieces, making your code organized, modular, and easier to maintain. Each component can have its own structure, logic, and style.",
-  },
-  {
-    src: PropsLogo,
-    heading: "State & Props",
-    para: "State and Props are fundamental for managing dynamic data in React. State holds data that can change over time within a component, while Props allow you to pass data from parent components to child components, enabling component reusability and flexibility.",
-  },
-  {
-    src: DomLogo,
-    heading: "Virtual DOM",
-    para: "The Virtual DOM is a lightweight copy of the actual DOM that React uses to optimize UI updates. When the state changes, React updates the Virtual DOM first, compares it with the real DOM, and efficiently updates only the necessary parts, improving performance and user experience.",
-  },
-];
+import BulbImage from "./bulb.svg"; // Nedd for second props method
+import { CoreData } from "./core-data.js";
 
 function Header() {
   return (
@@ -35,28 +16,48 @@ function Header() {
   );
 }
 
-function CoreConcept(prop) {
+// ++++++++++++++++++++++++++++++++++ TWO WAYS +++++++++++++++++++++++++++++++++
+
+// FIRST (Best)
+
+function CoreConcept({ src, heading, para }) {
   return (
-    <div class="core-concept">
-      <img src={prop.src} />
-      <h1>{prop.heading}</h1>
-      <p>{prop.para}</p>
+    <div className="core-concept">
+      <img src={src} alt={heading} />
+      <h1>{heading}</h1>
+      <p>{para}</p>
     </div>
   );
 }
 
+// SECOND
+
+// function CoreConcept(props) {
+//   return (
+//     <div className="core-concept">
+//       <img src={props.src} alt={props.heading} />
+//       <h1>{props.heading}</h1>
+//       <p>{props.para}</p>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
-    <div class="initial-wrapper">
+    <div className="initial-wrapper">
       <Header />
-      <h1 class="core-heading"> Core Concepts</h1>
-      <div class="core-concept-container">
+      <h1 className="core-heading"> Core Concepts</h1>
+      <div className="core-concept-container">
         <CoreConcept
-          src={CoreData[0].src}
-          heading={CoreData[0].heading}
-          para={CoreData[0].para}
+          src={BulbImage}
+          heading="Components"
+          para="Components are the building blocks of any React application. They let you break the UI into reusable pieces, making your code organized, modular, and easier to maintain. Each component can have its own structure, logic, and style."
         />
-        <CoreConcept {...CoreData[1]} />
+        <CoreConcept
+          src={CoreData[1].src}
+          heading={CoreData[1].heading}
+          para={CoreData[1].para}
+        />
         <CoreConcept {...CoreData[2]} />
       </div>
     </div>
---
+++ src/core-data.js
@@ -0,0 +1,21 @@
+import CoreLogo from "./bulb.svg";
+import PropsLogo from "./pops.svg";
+import DomLogo from "./dom.svg";
+
+export const CoreData = [
+  {
+    src: CoreLogo,
+    heading: "Components",
+    para: "Components are the building blocks of any React application. They let you break the UI into reusable pieces, making your code organized, modular, and easier to maintain. Each component can have its own structure, logic, and style.",
+  },
+  {
+    src: PropsLogo,
+    heading: "State & Props",
+    para: "State and Props are fundamental for managing dynamic data in React. State holds data that can change over time within a component, while Props allow you to pass data from parent components to child components, enabling component reusability and flexibility.",
+  },
+  {
+    src: DomLogo,
+    heading: "Virtual DOM",
+    para: "The Virtual DOM is a lightweight copy of the actual DOM that React uses to optimize UI updates. When the state changes, React updates the Virtual DOM first, compares it with the real DOM, and efficiently updates only the necessary parts, improving performance and user experience.",
+  },
+];
